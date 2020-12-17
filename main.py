cc11c87f1c129d2802f1f02fd703d65ca366c92d
Albert Pulinski <albertpulinski@gmail.com>
2020-12-17 21:26:59 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+print("NR 1")
+print("**********")
+gwiazdki = "**********"
+for i in range(9):
+    if i % 2 == 0:
+      print(gwiazdki)
+    else:
+      print(f" {gwiazdki}")
+
+print("NR 2")
+star = "*"
+for i in range(5):
+    print(star * (2 *i))
+    print(star * (2 *i))
+
+
+
+print("NR 3")
+for i in range(7,0,-1):
+    print("*" * (i))
